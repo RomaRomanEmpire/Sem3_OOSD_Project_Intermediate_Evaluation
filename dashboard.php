6b4617aef0476eb8ddfdf48feb9e9735c458bbeb
ThevinSenath <thevinsenath12345@gmail.com>
2022-01-19 13:48:32 +0530

    1.48 pm 19/01/2022

--- dashboard.php
+++ dashboard.php
@@ -13,16 +13,19 @@ if ($u_type === "applicant") {
     $already_applied = is_null($con->get_column_value("application_details", "applicant_id", "=", $_SESSION['user_id'], "app_id", ""));
 
     if (!$already_applied) {
-        echo "<style>#view:hover{
-            background-color: rgb(17, 0, 255);
+        echo "<style>#view_application_app:hover{
+            background-color: rgb(246, 245, 248);
+            color: #f05462;
     }</style>";
 
     } else {
-        echo "<style>#new:hover{
-            background-color: rgb(17, 0, 255);
+        echo "<style>#new_application:hover{
+            background-color: rgb(246, 245, 248);
+            color: #f05462;
         }</style>";
-        echo "<style>#lost:hover{
-            background-color: rgb(17, 0, 255);
+        echo "<style>#lost_application:hover{
+            background-color: rgb(246, 245, 248);
+            color: #f05462;
         }</style>";
     }
     echo "<style>#notification-panel1{
@@ -57,9 +60,18 @@ if($u_type!= 'db_manager'){
     <script src="https://kit.fontawesome.com/78dc5e953b.js" crossorigin="anonymous"></script>
     <style>
         h1{
-    font-size: 50px;
-    font-family: 'Times New Roman', Times, serif;
-}
+            font-size: 50px;
+            font-family: 'Times New Roman', Times, serif;
+        }
+
+        #view_application_db:hover,
+        #view_application_rap:hover,
+        #view_database:hover,
+        #view_notifications:hover{
+            background-color: rgb(246, 245, 248);
+            color: #f05462;
+        }
+
     </style>
 </head>
 
@@ -83,23 +95,17 @@ if($u_type!= 'db_manager'){
 </div>
 
 <div class="container" >
-    <!-- <img src="Image/Q.jpg"> -->
+
     <div class="header" style="background: rgba(0,0,0,0.5);">
 
         <div class="nav">
-            <!-- <div class="search">
-                 <button type="submit"  ><img src="Image/search.png" alt=""></button>
-                 <input type="text" name="" id="" placeholder="Search....">
-
-           </div> -->
             <div class="user">
 
 
                 <div class="img-case">
 
                 </div>
-                <!-- <a href=""  class="btn" >Notification</a>
-                <a href=""  class="btn"> Profile</a>   -->
+
                 <a href="logout.php" class="btn" ><i class="fas fa-sign-out-alt"></i> Log Out</a>
 
             </div>
@@ -113,19 +119,19 @@ if($u_type!= 'db_manager'){
                 <br><br><br><br>
                 <a href="GNDivisionSelect.php?id=<?php echo 1 ?>">
                     <!--                    <button>Applying New Identity Card</button>-->
-                    <button id="new"<?php if (!$already_applied) { ?> disabled <?php } ?>>
+                    <button id="new_application"<?php if (!$already_applied) { ?> disabled <?php } ?>>
                     <i class="fas fa-hand-point-right"></i> Applying New Identity Card
                     </button>
                 </a><br><br><br>
 
                 <a href="GNDivisionSelect.php?id=<?php echo 2 ?>">
-                    <button id="lost"<?php if (!$already_applied) { ?> disabled<?php } ?>> <i class="fas fa-hand-point-right"></i> Applying For lost Identity
+                    <button id="lost_application"<?php if (!$already_applied) { ?> disabled<?php } ?>> <i class="fas fa-hand-point-right"></i> Applying For lost Identity
                         Card
                     </button>
                 </a><br><br><br>
 
                 <a href="Filled_Application.php?application_id=<?php echo $user->getApplicationId() ?>">
-                    <button id="view"<?php if ($already_applied) { ?> disabled<?php } ?>>  <i class="fas fa-hand-point-right"></i> View application details in
+                    <button id="view_application_app"<?php if ($already_applied) { ?> disabled<?php } ?>>  <i class="fas fa-hand-point-right"></i> View application details in
                         process
                     </button>
                 </a>
@@ -134,15 +140,15 @@ if($u_type!= 'db_manager'){
                 <div id="db_manager-links" class="icon_case">
                     <br><br><br><br>
                     <a href="user_Details.php">
-                        <button>  <i class="fas fa-hand-point-right"></i> View Database Details</button>
+                        <button id="view_database">  <i class="fas fa-hand-point-right"></i> View Database Details</button>
                     </a>
                     <br><br><br>
                     <a href="View_Applications_Details.php">
-                        <button>  <i class="fas fa-hand-point-right"></i> View Application Details</button>
+                        <button id="view_application_db">  <i class="fas fa-hand-point-right"></i> View Application Details</button>
                     </a>
                     <br><br><br>
                     <a href="DBM_Notification.php">
-                        <button>  <i class="fas fa-hand-point-right"></i>View Notification Details</button>
+                        <button id="view_notifications">  <i class="fas fa-hand-point-right"></i>View Notification Details</button>
                     </a>
                     <br><br>
                 </div>
@@ -151,9 +157,8 @@ if($u_type!= 'db_manager'){
                 <div id="rap_links" class="icon_case">
                     <br><br><br><br>
                     <a href="View_Applications_Details.php">
-                        <button>  <i class="fas fa-hand-point-right"></i>View Applications Details</button>
+                        <button id="view_application_rap">  <i class="fas fa-hand-point-right"></i>View Applications Details</button>
                     </a><br><br>
-                    <!-- <button>Applying For lost Identity Card </button> -->
                 </div>
             </div>
         <?php } ?>
