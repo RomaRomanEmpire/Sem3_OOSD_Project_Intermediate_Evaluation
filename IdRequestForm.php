70df9054900f5b0973018beb7c081007ff034c46
HarshanaEranda <harshawije1998@gmail.com>
2022-01-19 15:52:49 +0530

    01/19 3.52 changed

--- IdRequestForm.php
+++ IdRequestForm.php
@@ -20,7 +20,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
         $applicant->set_row_id($_SESSION['user_id']);
 
-        $applicant->apply_NIC($_GET['basic'], $_GET['ds'], $_GET['table'], $application);
+        $applicant->apply_NIC($_GET['table'], $application);
     }
 }
 ?>
